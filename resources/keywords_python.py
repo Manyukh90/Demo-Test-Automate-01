2bde4d61e0523ae1b28b6fd63d86615862a2c866
Manyukh90 <92971601+Manyukh90@users.noreply.github.com>
2023-03-24 14:02:57 +0700

    Update keywords_python.py
    remove commit line 6

--- resources/keywords_python.py
+++ resources/keywords_python.py
@@ -2,5 +2,3 @@ def split_string(var):
     txt = var
     x = txt.split("/", 4)
     return  x
-
-# print(split_string("https://th.indeed.com/m/jobsq=Software+Tester&l=%E0%B8%9E%E0%B8%8D%E0%B8%B2%E0%B9%84%E0%B8%97")[2])
